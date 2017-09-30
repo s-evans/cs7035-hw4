ee8436bdd5463f4b337c878a1b856c81f15a9442
Sean Evans <seanevans>
2017-09-30 12:14:08 -0400

    update gitignore; implement and solve problem 2; start on problem 1; add word doc;

--- problem_1.cpp
+++ problem_1.cpp
@@ -3,8 +3,14 @@
 #include "gmpxx.h"
 #include "fermat_test.hpp"
 
+// 1) Factor the number n using the p – 1 factoring method, where
+// n = 190 248 273 382 547 686 244 479 775 579 416 295 505 415 044 511.
+// Here n=pq, where p and q are prime and p-1 is 37-smooth.
+
 int main()
 {
     // TODO: implement and solve
+    static const mpz_class n = 190248273382547686244479775579416295505415044511_mpz;
+
     return EXIT_SUCCESS;
 }
--- problem_2.cpp
+++ problem_2.cpp
@@ -1,10 +1,34 @@
 #include <cstdlib>
 #include <iostream>
 #include "gmpxx.h"
-#include "fermat_test.hpp"
+#include "fibonacci_test.hpp"
+#include "thousand_primes_test.hpp"
+#include <assert.h>
+
+// 2) Find the smallest safe prime p >= 3 x 10^15 + 11. [Only test p of the form 12k + 11.]
 
 int main()
 {
-    // TODO: implement and solve
-    return EXIT_SUCCESS;
+    // Solution:
+    // k = 250000000000009
+    // p = 3000000000000119
+
+    static const constexpr unsigned int offset = 11;
+    static const constexpr unsigned int step = 12;
+    static const mpz_class lower_bound = 3000000000000011_mpz;
+    static const mpz_class init_k = ( lower_bound - offset ) / step;
+
+    mpz_class possible_prime = step * init_k + offset;
+
+    for ( mpz_class k = init_k ; true ; ++k, possible_prime += step ) {
+        assert( possible_prime >= lower_bound );
+
+        if ( thousand_primes_test( possible_prime ) && fibonacci_test( possible_prime ) ) {
+            std::cout << "k = " << k << std::endl;
+            std::cout << "p = " << possible_prime << std::endl;
+            return EXIT_SUCCESS;
+        }
+    }
+
+    return EXIT_FAILURE;
 }
