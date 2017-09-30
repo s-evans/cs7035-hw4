729796690b2600a68ea0f77eb9f0b1acd624321b
Sean Evans <seanevans>
2017-09-30 11:34:02 -0400

    initialize repo with copies of utilities from previous homework assignment;

---
+++ identity.hpp
@@ -0,0 +1,12 @@
+#ifndef IDENTITY_H
+#define IDENTITY_H
+
+template<class T>
+struct identity {
+    using type = T;
+    static inline constexpr type get() {
+        return 1;
+    }
+};
+
+#endif // IDENTITY_H
---
+++ problem_1.cpp
@@ -0,0 +1,10 @@
+#include <cstdlib>
+#include <iostream>
+#include "gmpxx.h"
+#include "fermat_test.hpp"
+
+int main()
+{
+    // TODO: implement and solve
+    return EXIT_SUCCESS;
+}
